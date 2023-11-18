544cd494b8b0e7843078f04c8a636f843462885e
MonsieurMajy <90928733+MonsieurMajy@users.noreply.github.com>
2023-11-18 20:00:54 +0200

    Change import to export

--- deps.js
+++ deps.js
@@ -1,2 +1,2 @@
-import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
-import { configure, renderFile } from "https://deno.land/x/eta@v2.2.0/mod.ts";
+export { serve } from "https://deno.land/std@0.202.0/http/server.ts";
+export { configure, renderFile } from "https://deno.land/x/eta@v2.2.0/mod.ts";
